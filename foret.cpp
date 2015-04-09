ad910c60fd620d87fbc4c515a73a9a57c26fa55d
Florian David <florian.david@etud.univ-angers.fr>
2015-04-09 09:35:53 +0200

    test maj

--- foret.cpp
+++ foret.cpp
@@ -1,7 +1,8 @@
 #include "foret.h"
 
 #include <cstdlib>
-#include <bits/stl_algo.h>
+#include <curses.h> // temp
+// #include <bits/stl_algo.h>
 // #include <bits/algorithmfwd.h>
 
 using namespace std;
@@ -38,8 +39,9 @@ void Foret::initialisation(float probabilite)
 		matrice.push_back(tmp);
 	}
 	
-	matrice[lignes/2][0]= 2;
-	matrice[lignes/2+1][1]= 2;
+	
+	enflammer(lignes/2, 0);
+	enflammer(lignes/2+1, 1);
 	
 	// 	Il faudrait ecrire =Cellule(X) si il y a plusieurs arguments
 	// 	matrice[lignes/2][colonnes/2]= Cellule(1);
@@ -48,15 +50,62 @@ void Foret::initialisation(float probabilite)
 }
 
 
-void Foret::transition(Cellule cell)
+// ###################################
+// 	Modification des éléments
+// ###################################
+void Foret::enflammer(int x, int y)
 {
-	if (cell.getEtat()==2){
-// 				if ((j-1)!= matrice[i].begin())
-// 					*(j-1)= Cellule(2);
-			
-			cell= Cellule(3);
-// 			return true;
+	Cellule& tmp= matrice[x][y];
+	
+	if (tmp.getEtat()==1){
+		onFire.push_back(Coordonnee(x, y));
+		tmp.enflammer();
 	}
+}
+
+void Foret::enflammer(Coordonnee c)
+{
+	Cellule& tmp= matrice[c.x][c.y];
+	
+	if (tmp.getEtat()==1){
+		onFire.push_back(c);
+		tmp.enflammer();
+	}
+}
+
+
+void Foret::enflammer(Cellule& cell, int x, int y)
+{
+	cell.enflammer();
+	onFire.push_back(Coordonnee(x, y));
+}
+
+
+void Foret::eteindre(int x, int y)
+{
+	matrice[x][y].blast();
+// 	onFire.remove_if<Cellule>();
+}
+
+
+
+// ###################################
+//	Avancee du temps
+// ################################### 
+void Foret::transition(Cellule& cell, int x, int y)
+{
+// 	printw("Transition %d, %d ; ", x, y); refresh();
+// 	if (cell.isOnFire()) {
+		cell.blast();
+// 			return true;
+		
+		list<Coordonnee> voisins= adjacents(Coordonnee(x, y));
+		for (list<Coordonnee>::const_iterator c(voisins.begin()); c!=voisins.end(); ++c){
+			enflammer(*c);
+		}
+		
+// 	}
+	
 // 		return false;
 }
 
@@ -64,20 +113,55 @@ void Foret::transition(Cellule cell)
 bool Foret::NextMove()
 {
 	bool modif= false;
-	for (int i= 0; i< lignes; ++i){
-// 		for_each<Cellule>(matrice[i].begin(), matrice[i].end(), transition);
 	
-		for (vector<Cellule>::iterator j(matrice[i].begin()); j!=matrice[i].end(); ++j){
-			// 		utilise un booléen pour initialiser
-			if (j->getEtat()==2){
-// 				if ((j-1)!= matrice[i].begin())
-// 					*(j-1)= Cellule(2);
-					
-				*j= Cellule(3);
-				modif= true;
-			}
+// 		for_each<Cellule>(matrice[i].begin(), matrice[i].end(), transition);
+// 	for (int i= 0; i< lignes; ++i)
+	
+	if (!onFire.empty()){
+		modif= true;
+		
+		list<Coordonnee> old= onFire;
+		onFire.clear();
+	
+		// TODO utiliser mapping
+		for (list<Coordonnee>::const_iterator cell(old.begin()); cell!=old.end(); ++cell){
+			int x= cell->x;
+			int y= cell->y;
+			transition(matrice[x][y], x, y);
 		}
 	}
 	
 	return modif;
+}
+
+
+
+// ###################################
+//	Acces aux elements
+// ################################### 
+vector< Cellule > Foret::operator[](int x)
+{
+	return matrice[x];
+}
+
+
+list< Coordonnee > Foret::adjacents(const Coordonnee& coord) const
+{
+	int x= coord.x;
+	int y= coord.y;
+	
+	list< Coordonnee > liste;
+	if (x<colonnes-1)
+		liste.push_back(Coordonnee(x+1, y));
+	
+	if (x>0)
+		liste.push_back(Coordonnee(x-1, y));
+
+	if (y<lignes-1)
+		liste.push_back(Coordonnee(x, y+1));
+	
+	if (y>0)
+		liste.push_back(Coordonnee(x, y-1));
+	
+	return liste;
 }
