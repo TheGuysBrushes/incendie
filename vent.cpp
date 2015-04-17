328cbd3a5dce45f7bd468100197e3e27a5ae27bf
Florian David <fl.david.53@gmail.com>
2015-04-17 17:01:43 +0200

    ajout vent

---
+++ vent.cpp
@@ -0,0 +1,80 @@
+#include "vent.h"
+
+using namespace std;
+
+Vent::Vent(float NordSud, float EstOuest)
+	:NS(NordSud) , EW(EstOuest)
+{
+}
+
+Vent::Vent(const Vent& other)
+	: NS(other.NS), EW(other.EW)
+{
+}
+
+Vent::~Vent()
+{
+
+}
+
+
+// ####################
+// 	OPERATIONS
+// ####################
+
+Vent& Vent::operator+(const Vent& other)
+{
+	return *(new Vent(NS + other.NS, EW + other.EW));
+}
+
+Vent& Vent::operator-(const Vent& other)
+{
+	return *(new Vent(NS - other.NS, EW - other.EW));
+}
+
+Vent& Vent::operator*(float coefficient)
+{
+	return *(new Vent(NS*coefficient, EW*coefficient));
+}
+
+Vent& Vent::operator/(float coefficient)
+{
+	return *(new Vent(NS/coefficient, EW/coefficient));
+}
+
+
+
+// ####################
+// 	ASSIGNATIONS
+// ####################
+
+Vent& Vent::operator=(const Vent& other)
+{
+	return *this;
+}
+
+
+Vent& Vent::operator+=(const Vent& other)
+{
+		NS += other.NS;
+		EW += other.EW;
+		
+		return *this;
+}
+
+std::string Vent::toString() const
+{
+	ostringstream os;
+
+	os << "(Force Nord : "<< NS<< " Force Est : "<< EW << ")";
+	return os.str();
+}
+
+// ####################
+// 	AUTRES CLASSES
+// ####################
+ostream& operator<<(ostream& os, const Vent& vent)
+{
+	os<< vent.toString();
+	return os;
+}
