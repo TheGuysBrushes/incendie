0567ca2e1a1891acb169ac0ba03afeab5b838bc2
Seymour49 <ugo.rayer@laposte.net>
2015-04-23 14:59:53 +0200

    fwelcome

---
+++ fwelcome.cpp
@@ -0,0 +1,82 @@
+#include "fwelcome.h"
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QGridLayout>
+
+Fwelcome::Fwelcome(QWidget* parent): QDialog(parent)
+{
+	// Initialisation des composants
+	QString s = " Bienvenue sur l'automate de simulation de feux de foret. \n Veuillez renseigner les differents parametres pour la simulation \n que vous allez effectuer, puis validez. \n";
+	QLabel* present = new QLabel(s);
+	present->setWordWrap(true);
+	
+	QLabel* h_lbl = new QLabel("Hauteur : ");
+	QLabel* l_lbl = new QLabel("Largeur : ");
+	QLabel* p_lbl = new QLabel("Probabilite : ");
+	QLabel* c_lbl = new QLabel("Coefficient : ");
+	
+	haut_edit = new QLineEdit("50");
+	larg_edit = new QLineEdit("50");
+	
+	QSlider * slide_p = new QSlider(Qt::Horizontal,0);
+	QSlider* slide_c = new QSlider(Qt::Horizontal,0);
+	slide_c->setMaximum(100);
+	slide_c->setMinimum(1);
+	slide_p->setMaximum(100);
+	slide_p->setMinimum(1);
+	
+	slide_c->setValue(50);
+	slide_p->setValue(50);
+	
+	p_value = new QLabel("0.50");
+	c_value = new QLabel("0.50");
+	
+	valid_btn = new QPushButton("Valider");
+	// Encapsulation des éléments
+	QVBoxLayout* lay = new QVBoxLayout(this);
+	QWidget* ww = new QWidget(this);
+	QGridLayout* grid_lay = new QGridLayout(ww);
+	
+	grid_lay->addWidget(h_lbl,0,0);
+	grid_lay->addWidget(haut_edit,0,1);
+	grid_lay->addWidget(l_lbl,1,0);
+	grid_lay->addWidget(larg_edit,1,1);
+	grid_lay->addWidget(p_lbl,2,0);
+	grid_lay->addWidget(slide_p,2,1);
+	grid_lay->addWidget(p_value,2,2);
+	grid_lay->addWidget(c_lbl,3,0);
+	grid_lay->addWidget(slide_c,3,1);
+	grid_lay->addWidget(c_value,3,2);
+	
+	lay->addWidget(present);
+	lay->addWidget(ww);
+	lay->addWidget(valid_btn);
+	
+	
+	// Connection entre Slider et Label associé
+	connect(slide_c, SIGNAL(valueChanged(int)), this, SLOT(set_coef(int)) );
+	connect(slide_p, SIGNAL(valueChanged(int)), this, SLOT(set_proba(int)) );
+	
+}
+
+Fwelcome::~Fwelcome()
+{
+
+}
+
+/*** SLOTS ***/
+void Fwelcome::set_proba(int x)
+{
+	proba = (float) x/100;
+	p_value->setText(QString::number(proba));
+}
+
+void Fwelcome::set_coef(int x)
+{
+	coef_brulure = (float) x/100;
+	c_value->setText(QString::number(coef_brulure));
+}
+
+
+
+
