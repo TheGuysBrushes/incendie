4a04997273ade903b4749459da6715b6fa41dfa9
Seymour49 <ugo.rayer@laposte.net>
2015-04-28 14:36:50 +0200

    Modif vent

--- Moteur/vent.cpp
+++ Moteur/vent.cpp
@@ -3,13 +3,13 @@
 
 using namespace std;
 
-Vent::Vent(float NordSud, float EstOuest)
-	:NS(NordSud) , EW(EstOuest)
+Vent::Vent(float _power_h, float _power_v)
+	:power_h(_power_h),power_v(_power_v)
 {
 }
 
 Vent::Vent(const Vent& other)
-	: NS(other.NS), EW(other.EW)
+	: power_h(other.power_h), power_v(other.power_v)
 {
 }
 
@@ -22,22 +22,22 @@ Vent::Vent(const Vent& other)
 
 Vent& Vent::operator+(const Vent& other)
 {
-	return *(new Vent(NS + other.NS, EW + other.EW));
+	return *(new Vent(power_v + other.power_v, power_h + other.power_h));
 }
 
 Vent& Vent::operator-(const Vent& other)
 {
-	return *(new Vent(NS - other.NS, EW - other.EW));
+	return *(new Vent(power_v - other.power_v, power_h - other.power_h));
 }
 
 Vent& Vent::operator*(float coefficient)
 {
-	return *(new Vent(NS*coefficient, EW*coefficient));
+	return *(new Vent(power_v*coefficient, power_h*coefficient));
 }
 
 Vent& Vent::operator/(float coefficient)
 {
-	return *(new Vent(NS/coefficient, EW/coefficient));
+	return *(new Vent(power_v/coefficient, power_h/coefficient));
 }
 
 
@@ -54,8 +54,8 @@ Vent& Vent::operator=(const Vent& other)
 
 Vent& Vent::operator+=(const Vent& other)
 {
-		NS += other.NS;
-		EW += other.EW;
+		power_v += other.power_v;
+		power_h += other.power_h;
 		
 		return *this;
 }
@@ -64,13 +64,13 @@ std::string Vent::toString() const
 {
 	ostringstream os;
 
-	os << "(Force Nord : "<< NS<< " Force Est : "<< EW << ")";
+	os << "(Force Nord : "<< power_v<< " Force Est : "<< power_h << ")";
 	return os.str();
 }
 
 int Vent::getSpeed() const
 {
-	return sqrt( (NS*NS) + (EW*EW) ) *50.0;
+	return sqrt( (power_v*power_v) + (power_h*power_h) ) *50.0;
 }
 
 
